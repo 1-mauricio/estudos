d9e4cdad074a158515a91b483dc1d96ac25771c9
1-mauricio <mauriiciioallves@hotmail.com>
2021-09-30 14:53:19 -0300

    update

---
+++ Algoritmos-EstruturaDeDados-UFPE/iudex/list1/01/script.py
@@ -0,0 +1,15 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class Pilha:
+    def __init__(self, N):
+        self.top = Node
+
+    def info(self, preco, dia):
+        return f"o ultimo valor foi {preco} e hoje e um bom dia para vender acoes dos ultimos {dia} dias"
+
+
+if __name__ == "__main__":
+    pass
