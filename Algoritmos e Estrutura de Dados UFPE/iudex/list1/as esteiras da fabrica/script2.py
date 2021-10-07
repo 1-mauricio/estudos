96330a18d0fdefd834d695caeef9fee79502d391
1-mauricio <mauriiciioallves@hotmail.com>
2021-10-07 16:44:27 -0300

    update

--- Algoritmos e Estrutura de Dados UFPE/iudex/list1/as esteiras da fabrica/script2.py
+++ Algoritmos e Estrutura de Dados UFPE/iudex/list1/as esteiras da fabrica/script2.py
@@ -1,5 +1,3 @@
-import sys
-
 class NoProduto:
     def __init__(self, produto) -> None:
         self.produto = produto
@@ -10,7 +8,7 @@ class Esteira:
         self.primeiro = None
         self.ultimo = None
 
-    def insert(self, produto):
+    def insert(self, produto, test):
         novo_node = NoProduto(produto)
         if self.primeiro == None:
             self.primeiro = novo_node
@@ -19,15 +17,19 @@ class Esteira:
             self.ultimo.proximo = novo_node
             self.ultimo = novo_node
 
-        return novo_node.produto
+        if test == 'move':
+            novo_node.proximo = None
 
     def remove(self):
         if self.primeiro != None:
             aux = self.primeiro
             self.primeiro = aux.proximo
+            aux.proximo = None
+            if self.primeiro == None:
+                return 1, aux.produto
             return aux.produto
         else:
-            print("Nao ha produtos para empacotar na esteira I!")
+            return 0
 
     def show(self):
         if self.primeiro != None:
@@ -36,9 +38,9 @@ class Esteira:
             while aux != None:
                 resultado += str(aux.produto) + " "
                 aux = aux.proximo 
-            print(resultado)
+            print(resultado.strip())
         else:
-            print("fila vazia")
+            print()
 
 if __name__ == "__main__":
     fabrica = []
@@ -47,9 +49,8 @@ if __name__ == "__main__":
     while True:
         if i == 0:
             qtd = int(input())
-            while int(qtd) >= 1:
+            for i in range(qtd, 0, -1):
                 fabrica.append(Esteira())  
-                qtd -= 1  
             i = 1
         else:
             entrada = input()
@@ -58,31 +59,45 @@ if __name__ == "__main__":
                 numero = int(entrada[1])
                 esteira = fabrica[numero-1]
                 produto = entrada[2]
-                esteira.insert(produto)
-                print('O produto: "{produto}" foi adicionado na esteira {numero}'.format(produto = produto, numero = numero))
+                esteira.insert(produto, 0)
+                print('O produto: "{produto}" foi adicionado na esteira {numero}.'.format(produto = produto, numero = numero))
+
             elif "RMV" in entrada:
                 entrada = entrada.split()
                 numero = int(entrada[1])
                 esteira = fabrica[numero-1]
                 produto = esteira.remove()
-                print("O produto: ", produto," foi empacotado com sucesso!")
+                if produto == 0:
+                    print("Nao ha produtos para empacotar na esteira {numero}!" .format(numero = entrada[1]))
+                elif produto[0] == 1:
+                    print('O produto: "{produto}" foi empacotado com sucesso!' .format(produto = produto[-1]))
+                    print("A esteira {numero} ficou vazia..." .format(numero = entrada[1]))
+                else:
+                    print('O produto: "{produto}" foi empacotado com sucesso!' .format(produto = produto))
+
             elif "MOV" in entrada:
                 entrada = entrada.split()
-                numero1 = int(entrada[1])
-                numero2 = int(entrada[2])
-                esteira1 = fabrica[numero1-1]
-                esteira2 = fabrica[numero2-1]
-                esteira2.insert(esteira1.primeiro)
-                produto = esteira1.remove()
-                print('O produto: "{produto}" foi movido para a esteira {numero2}' .format(produto = produto, numero2 = numero2))
+                numero_esteira_rem = int(entrada[1])
+                numero_esteira_add = int(entrada[2])
+                esteira_rem = fabrica[numero_esteira_rem-1]
+                esteira_add = fabrica[numero_esteira_add-1]
+                if esteira_rem.primeiro != None:
+                    aux = esteira_rem.primeiro.produto
+            
+                    esteira_add.insert(aux, 'move')
+                    esteira_rem.remove()
+                    print('O produto: "{produto}" foi movido para a esteira {numero}.' .format(produto = aux, numero = numero_esteira_add))
+                else:
+                    print("Erro ao mover produto.")
                 
             elif "SHW" in entrada:
                 entrada = entrada.split()
                 esteira = fabrica[int(entrada[1])-1]
+                print("Esteira {numero}:" .format(numero=entrada[1]))
                 esteira.show()
+
             elif entrada == "END":
                 exit()
-    
 
     '''i = 0
     for line in sys.stdin:
