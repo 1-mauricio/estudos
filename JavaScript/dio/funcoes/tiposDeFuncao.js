ae469cc7c4264235a552be037ae3051d162c66cf
mauriciomasj <mauricioalves.masj@gmail.com>
2022-01-12 23:46:26 -0300

    add ReactJs

---
+++ JavaScript/dio/funcoes/tiposDeFuncao.js
@@ -0,0 +1,16 @@
+//declarativas
+
+function declarativa() {
+    console.log("função declarativa")
+}
+
+//com nomeação
+var funcao = function() {
+    console.log("função com nomeacao")
+}
+
+//arrow function
+
+var arrowFunc = () => {
+    console.log("arrow function")
+}
